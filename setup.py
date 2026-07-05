d42899b320613374b411e2fc5aa6d8cc08d563d8
Prahlad Yeri <prahladyeri@yahoo.com>
2026-07-05 17:32:23 +0530

    chore: updated setup.py, screenshot

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from gubbi import __version__, PKG_NAME
+#from gubbi import __version__, PKG_NAME
+import re
 from pathlib import Path
 from setuptools import setup, find_packages
+
+version_file = Path(__file__).parent / "src" / "gubbi" / "__init__.py"
+version_match = re.search(r'^__version__\s*=\s*[\'"]([^\'"]+)[\'"]', version_file.read_text(), re.M)
+version = version_match.group(1)
 
 HERE = Path(__file__).parent
 
 setup(
-    name=PKG_NAME,
-    version=__version__,
+    name="gubbi",
+    version=version,
     description="Minimalist terminal LLM chatbot",
     long_description=(HERE / "README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
@@ -21,6 +26,7 @@ setup(
         "requests>=2.32.3",
         "openai>=1.109,<2",
         "colorama>=0.4.6",
+        "keyring>=25.5.0"
     ],
 
     entry_points={
